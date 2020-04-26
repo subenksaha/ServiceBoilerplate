a64559e27ec72efdedd3d771c4e99dbdd4e0948b
Suben K. Saha <subenksaha@gmail.com>
2020-04-26 19:12:35 +0600

    Added license

--- src/configs/winston.config.ts
+++ src/configs/winston.config.ts
@@ -1,19 +1,38 @@
 import { registerAs } from "@nestjs/config";
 import * as winston from 'winston'
-import { utilities } from "nest-winston";
+import { utilities, WinstonModuleOptions } from "nest-winston";
 
 export default registerAs('winston', () => {
+    const common = {
+        defaultMeta: {
+            service: 'service'
+        },
+        exitOnError: false,
+    } as WinstonModuleOptions;
+
     const TransportConsole = new winston.transports.Console({
+        handleExceptions: true,
         format: winston.format.combine(
             winston.format.timestamp(),
             utilities.format.nestLike()
         )
+    });
+    const TransportFile = new winston.transports.File({
+        handleExceptions: true,
+        filename: 'logs/log',
+        format: winston.format.combine(
+            winston.format.timestamp(),
+        )
     })
     return process.env.NODE_ENV == 'DEV'? {
+        ...common,
         transports: [
             TransportConsole
         ]
     } : {
-        transports: []
+        ...common,
+        transports: [
+            TransportFile
+        ]
     }
 })
--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
-import { WINSTON_MODULE_NEST_PROVIDER } from 'nest-winston';
+import { WINSTON_MODULE_NEST_PROVIDER, WinstonModule } from 'nest-winston';
+import winstonConfig from './configs/winston.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
-    logger: false
+    logger: WinstonModule.createLogger(winstonConfig())
   });
   const configService = app.get(ConfigService);
   const winston = app.get(WINSTON_MODULE_NEST_PROVIDER);
-  app.useLogger(winston);
   await app.listen(configService.get<number>('PORT'));
   winston.logger.info(`Server started @ ${await app.getUrl()}`)
 }
